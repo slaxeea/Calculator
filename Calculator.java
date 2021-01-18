8896ee22be1c40221bbe4c13ddc4d7c3bc52562f
simon <kappelers77@gmail.com>
2021-01-18 14:59:25 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,9 +20,6 @@ public class Calculator extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
